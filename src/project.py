778e54babb57d73b4263c9fa2b767bc33813cd43
Arwen Varner <afv220001@utdallas.edu>
2023-11-16 20:48:45 -0600

    Resolution and Ratio section (functioning)

--- src/project.py
+++ src/project.py
@@ -1,1 +1,22 @@
-#Empty Project.py file
+from PIL import Image
+import os
+
+def analyze_image(image_filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(script_dir, image_filename)
+    
+    try:
+        image = Image.open(image_path)
+        width, height = image.size
+        ratio = width / height
+        print("Image Resolution:", width, "x", height)
+        print("Image Ratio:", ratio)
+    except FileNotFoundError:
+        print("Image file not found. Please make sure the file exists.")
+
+# Prompt the user to enter the image filename
+image_filename = input("Enter the image filename: ")
+
+# Call the analyze_image function with the user-provided image filename
+analyze_image(image_filename)
+
