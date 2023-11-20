bd5146614c4c9a3afd6990a61975ac7e5a419594
Arwen Varner <afv220001@utdallas.edu>
2023-11-20 11:46:41 -0600

    Working options of color profile/image info

--- src/project.py
+++ src/project.py
@@ -1,7 +1,34 @@
 from PIL import Image
 import os
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
+def most_dominant_colors(img):
+    resized_img = img.resize((100, 100))
+    rgb_img = resized_img.convert("RGB")
+    pixel_colors = np.array(rgb_img).reshape(-1, 3)
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(pixel_colors)
+    dominant_colors = kmeans.cluster_centers_.astype(int)
+    return dominant_colors
 
+def create_color_samples(colors):
+    fig, axs = plt.subplots(1, 5, figsize=(12, 2))
+    for i, color in enumerate(colors):
+        axs[i].imshow([[color]], aspect='auto')
+        axs[i].axis('off')
+    plt.show()
+
+def analyze_image(image_filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    image_path = os.path.join(script_dir, image_filename)
+    try:
+        image = Image.open(image_path)
+        dominant_colors = most_dominant_colors(image)
+        create_color_samples(dominant_colors)
+    except FileNotFoundError:
+        print("Image file not found. Please make sure the file exists.")
 
 def get_image_resolution(image):
     width, height = image.size
@@ -12,46 +39,22 @@ def get_image_ratio(image):
     ratio = width / height
     return ratio
 
-def analyze_image(image_filename):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    image_path = os.path.join(script_dir, image_filename)
-
-    try:
-        image = Image.open(image_path)
-        resolution = get_image_resolution(image)
-        ratio = get_image_ratio(image)
-
-        print("Image Resolution:", resolution[0], "x", resolution[1])
-        print("Image Ratio:", ratio)
-    except FileNotFoundError:
-        print("Image file not found. Please make sure the file exists.")
-
-# Prompt the user to enter the image filename
-image_filename = input("Enter the image filename: ")
-
-def main():
-
-    print("Welcome to the Image Insights Menu")
-    print("Select 1 to analyze Color Profile")
-    print("Select 2 to analyze Image Information")
-
-    choice = input("Enter your choice here: ")
+def menu():
+    print("Menu:")
+    print("1. Color Profile")
+    print("2. Image Info")
+    choice = input("Enter your choice: ")
 
     if choice == "1":
-        print("You chose analyze Color Profile!")
-        # call respective code here from class above
-        color_profile = Color_Profile()
-        color_profile.open_image_color(image)
-    
-
-    elif choice == "2":
-        print("You chose analyze Image Information")
-        # Call the analyze_image function with the user-provided image filename
+        image_filename = input("Enter the image filename: ")
         analyze_image(image_filename)
-
-    else:
-        print("Invalid choice. Please choose either '1' or '2.'")
-
-if __name__ == "__main__":
-    main()
-
+    elif choice == "2":
+        image_filename = input("Enter the image filename: ")
+        image = Image.open(image_filename)
+        resolution = get_image_resolution(image)
+        ratio = get_image_ratio(image)
+        print("Image resolution:", resolution)
+    elif choice == "3":
+        print("Invalid Choice. Please choose either 1 or 2")
+        
+menu()
